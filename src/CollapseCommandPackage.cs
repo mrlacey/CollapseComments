63b3686b5709561eb39edb2b136a5646763a2a67
Matt Lacey <matt@mrlacey.co.uk>
2022-01-22 12:57:28 +0000

    Use VsctGenerator & improve package loading

--- src/CollapseCommandPackage.cs
+++ src/CollapseCommandPackage.cs
@@ -2,28 +2,24 @@
 // Licensed under the MIT license.
 
 using System;
-using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
 using System.Threading;
 using EnvDTE;
 using EnvDTE80;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
+using static Microsoft.VisualStudio.VSConstants;
 using Task = System.Threading.Tasks.Task;
 
 namespace CollapseComments
 {
-    [ProvideAutoLoad(UIContextGuids.SolutionHasMultipleProjects, PackageAutoLoadFlags.BackgroundLoad)]
-    [ProvideAutoLoad(UIContextGuids.SolutionHasSingleProject, PackageAutoLoadFlags.BackgroundLoad)]
+    [ProvideAutoLoad(UICONTEXT.CSharpProject_string, PackageAutoLoadFlags.BackgroundLoad)]
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)] // Info on this package for Help/About
     [ProvideMenuResource("Menus.ctmenu", 1)]
-    [Guid(CollapseCommandPackage.PackageGuidString)]
+    [Guid(PackageGuids.guidCollapseCommandPackageString)]
     [ProvideOptionPage(typeof(OptionsPageGrid), Vsix.Name, "General", 0, 0, true)]
     public sealed class CollapseCommandPackage : AsyncPackage
     {
-        public const string PackageGuidString = "02438993-d9fa-42ae-b30e-c4058e2136b3";
-
         public OptionsPageGrid Options
         {
             get
