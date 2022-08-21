7962ab7d50d52b94eb77b4e466eea2f841fc4b18
Matt Lacey <matt@mrlacey.co.uk>
2022-08-21 15:13:36 +0100

    v2.1.1 - update sponsor request message

--- src/SponsorRequestHelper.cs
+++ src/SponsorRequestHelper.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) Matt Lacey Ltd. All rights reserved.
 // Licensed under the MIT license.
 
-using System;
 using Microsoft.VisualStudio.Shell;
+using System;
 using Task = System.Threading.Tasks.Task;
 
 namespace CollapseComments
@@ -44,7 +44,7 @@ namespace CollapseComments
 
             GeneralOutputPane.Instance.WriteLine("Sorry to interrupt. I know your time is busy, presumably that's why you installed this extension (Collapse Comments).");
             GeneralOutputPane.Instance.WriteLine("I'm happy that the extensions I've created have been able to help you and many others");
-            GeneralOutputPane.Instance.WriteLine("but I also need to make a living, and two years without work and extended periods of illness have been a challenge. - I didn't qualify for any government support either. :(");
+            GeneralOutputPane.Instance.WriteLine("but I also need to make a living, and limited paid work over the last few years has been a challenge. :(");
             GeneralOutputPane.Instance.WriteLine(string.Empty);
             GeneralOutputPane.Instance.WriteLine("Show your support by making a one-off or recurring donation at https://github.com/sponsors/mrlacey");
             GeneralOutputPane.Instance.WriteLine(string.Empty);
