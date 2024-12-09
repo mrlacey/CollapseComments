8c29a059d94f5dab828a08bb241bc58a9d9239d9
Matt Lacey <matt@mrlacey.co.uk>
2024-12-09 07:40:23 +0000

    update analytics data

--- src/CollapseCommandPackage.cs
+++ src/CollapseCommandPackage.cs
@@ -82,6 +82,8 @@ namespace CollapseComments
 				{
 					{ "VsixVersion", Vsix.Version },
 					{ "VsVersion", Microsoft.VisualStudio.Telemetry.TelemetryService.DefaultSession?.GetSharedProperty("VS.Core.ExeVersion") },
+					{ "Architecture", RuntimeInformation.ProcessArchitecture.ToString() },
+					{ "MsInternal", Microsoft.VisualStudio.Telemetry.TelemetryService.DefaultSession?.IsUserMicrosoftInternal.ToString() },
 				};
 
 				client.TrackEvent(Vsix.Name, properties);
