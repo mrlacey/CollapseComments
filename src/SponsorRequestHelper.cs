b12abd3c1dd0782f67afd636e5f4a5e83a7bd900
Matt Lacey <matt@mrlacey.co.uk>
2025-03-19 17:36:11 +0000

    update sponsor request message

--- src/SponsorRequestHelper.cs
+++ src/SponsorRequestHelper.cs
@@ -7,63 +7,77 @@ using Task = System.Threading.Tasks.Task;
 
 namespace CollapseComments
 {
-    public class SponsorRequestHelper
-    {
-        public static async Task CheckIfNeedToShowAsync()
-        {
-            var settings = await InternalSettings.GetLiveInstanceAsync();
-            if (settings.FirstUse == DateTime.MinValue)
-            {
-                // Track first known use so don't prompt too soon
-                settings.FirstUse = DateTime.UtcNow;
-                await settings.SaveAsync();
-            }
+	public class SponsorRequestHelper
+	{
+	public static async Task CheckIfNeedToShowAsync()
+	{
+		var settings = await InternalSettings.GetLiveInstanceAsync();
+		if (settings.FirstUse == DateTime.MinValue)
+		{
+			// Track first known use so don't prompt too soon
+			settings.FirstUse = DateTime.UtcNow;
+			await settings.SaveAsync();
+		}
 
-            if (await SponsorDetector.IsSponsorAsync())
-            {
-                if (new Random().Next(1, 10) == 2)
-                {
-                    await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+	  if (await SponsorDetector.IsSponsorAsync())
+	  {
+		if (new Random().Next(1, 10) == 2)
+		{
+		  await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
 
-                    ShowThanksForSponsorshipMessage();
-                }
-            }
-            else
-            {
-                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+		  ShowThanksForSponsorshipMessage();
+		}
+	  }
+	  else
+	  {
+		await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
 
-                ShowPromptForSponsorship();
+		ShowPromptForSponsorship();
 
-                if (settings.FirstUse < DateTime.UtcNow.AddDays(7)
-                    && settings.UseCount > 50)
-                {
-                    OutputPane.Instance.Activate();
-                }
-            }
-        }
+		if (settings.FirstUse < DateTime.UtcNow.AddDays(7)
+			&& settings.UseCount > 50)
+		{
+		  OutputPane.Instance.Activate();
+		}
+	  }
+	}
 
-        private static void ShowThanksForSponsorshipMessage()
-        {
-            ThreadHelper.ThrowIfNotOnUIThread();
+	private static void ShowThanksForSponsorshipMessage()
+	{
+	  ThreadHelper.ThrowIfNotOnUIThread();
 
-            OutputPane.Instance.WriteLine("Thank you for your sponsorship. It really helps.");
-            OutputPane.Instance.WriteLine("If you have ideas for new features or suggestions for new features");
-            OutputPane.Instance.WriteLine("please raise an issue at https://github.com/mrlacey/CollapseComments/issues");
-            OutputPane.Instance.WriteLine(string.Empty);
-        }
+	  OutputPane.Instance.WriteLine("Thank you for your sponsorship. It really helps.");
+	  OutputPane.Instance.WriteLine("If you have ideas for new features or suggestions for new features");
+	  OutputPane.Instance.WriteLine("please raise an issue at https://github.com/mrlacey/CollapseComments/issues");
+	  OutputPane.Instance.WriteLine(string.Empty);
+	}
 
-        private static void ShowPromptForSponsorship()
-        {
-            ThreadHelper.ThrowIfNotOnUIThread();
+	private static void ShowPromptForSponsorship()
+	{
+	  ThreadHelper.ThrowIfNotOnUIThread();
 
-            OutputPane.Instance.WriteLine("Sorry to interrupt. I know your time is busy, presumably that's why you installed this extension.");
-            OutputPane.Instance.WriteLine("I'm happy that the extensions I've created have been able to help you and many others");
-            OutputPane.Instance.WriteLine("but I also need to make a living, and limited paid work over the last few years has been a challenge. :(");
-            OutputPane.Instance.WriteLine(string.Empty);
-            OutputPane.Instance.WriteLine("Show your support by making a one-off or recurring donation at https://github.com/sponsors/mrlacey");
-            OutputPane.Instance.WriteLine(string.Empty);
-            OutputPane.Instance.WriteLine("If you become a sponsor, I'll tell you how to hide this message too. ;)");
-            OutputPane.Instance.WriteLine(string.Empty);
-        }
-    }
+	  OutputPane.Instance.WriteLine("********************************************************************************************************");
+	  OutputPane.Instance.WriteLine("This is a free extension that is made possible thanks to the kind and generous donations of:");
+	  OutputPane.Instance.WriteLine("");
+	  OutputPane.Instance.WriteLine("Daniel, James, Mike, Bill, unicorns39283, Martin, Richard, Alan, Howard, Mike, Dave, Joe, ");
+	  OutputPane.Instance.WriteLine("Alvin, Anders, Melvyn, Nik, Kevin, Richard, Orien, Shmueli, Gabriel, Martin, Neil, Daniel, ");
+	  OutputPane.Instance.WriteLine("Victor, Uno, Paula, Tom, Nick, Niki, chasingcode, luatnt, holeow, logarrhythmic, kokolorix, ");
+	  OutputPane.Instance.WriteLine("Guiorgy, Jessé, pharmacyhalo, MXM-7, atexinspect, João, hals1010, WTD-leachA, andermikael, ");
+	  OutputPane.Instance.WriteLine("spudwa, Cleroth, relentless-dev-purchases & 20+ more");
+	  OutputPane.Instance.WriteLine("");
+	  OutputPane.Instance.WriteLine("Join them to show you appreciation and ensure future maintenance and development by becoming a sponsor.");
+	  OutputPane.Instance.WriteLine("");
+	  OutputPane.Instance.WriteLine("Go to https://github.com/sponsors/mrlacey");
+	  OutputPane.Instance.WriteLine("");
+	  OutputPane.Instance.WriteLine("Any amount, as either a one-off or on a monthly basis, is appreciated more than you can imagine.");
+	  OutputPane.Instance.WriteLine("");
+	  OutputPane.Instance.WriteLine("I'll also tell you how to hide this message too.  ;)");
+	  OutputPane.Instance.WriteLine("");
+	  OutputPane.Instance.WriteLine("");
+	  OutputPane.Instance.WriteLine("If you can't afford to support financially, you can always");
+	  OutputPane.Instance.WriteLine("leave a positive review at https://marketplace.visualstudio.com/items?itemName=MattLaceyLtd.CollapseComments&ssr=false#review-details");
+	  OutputPane.Instance.WriteLine("");
+	  OutputPane.Instance.WriteLine("********************************************************************************************************");
+	}
+  }
 }
